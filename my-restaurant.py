75fc04707bc45f17687eb2c1f427407cf3d0bfb9
AbisheK0726 <abishekaneese726@gmail.com>
2022-10-12 17:24:55 +0100

    Removed repeated code

--- my-restaurant.py
+++ my-restaurant.py
@@ -3,9 +3,9 @@
 # As a user, I want to have my order read back to me in formated way so I know what I ordered.
 
 #create a dictionary of menu items
-
 menu = {1: "steak", 2: "pizza", 3: "salad", 4: "chicken", 5: "pasta", 6: "tofu", 7: "burger", 8: "sandwich", 9: "soup", 10: "ice cream"}
 
+# Welcome users and print the menu
 print ("\nWelcome to the restaurant! Here is our menu: ")
 for key, value in menu.items():
     print (key, "-" , value)
@@ -13,26 +13,27 @@ for key, value in menu.items():
 #ask user up to 3 orders
 order = {}
 
+def yourOrder():
+    print("=====================================")
+    print("Your order is: ")
+    for key, value in order.items():
+        print (key, "-" , value)
+    print("=====================================")
+
 while True:
         
     order_number = int(input("What would you like to order? "))
-    order[order_number] = menu[order_number]
+    order[order_number] = menu[order_number] #add item to new order dictionary
     
-    if len(order) == 3:
-        print ("Your order is: ")
-        for key, value in order.items():
-            print (key, "-" , value)
+    if len(order) == 3: #if 3 items have been ordered, break out of loop and print order
+        yourOrder()
         break
     
-    order_again = input("Would you like to order anything else? (yes/no) ")
+    order_again = input("Would you like to order anything else? (yes/no) ") #ask if user wants to order again
     if order_again == "yes":
         continue
     elif order_again == "no":
-        print("=====================================")
-        print ("Your order is: ")
-        for key, value in order.items():
-            print (key, "-" , value)
-            print("=====================================")
+        yourOrder()
         break
     else:
         print ("Please enter yes or no")
